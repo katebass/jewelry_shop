ce5a172d0f173d80079252f99b3513fe904d1995
Kateryna Basova <katy.bas@mail.ru>
2020-08-19 23:53:53 +0200

    add logger

--- src/main/java/com/edu/jewelry/controller/rest/MaterialRestController.java
+++ src/main/java/com/edu/jewelry/controller/rest/MaterialRestController.java
@@ -6,6 +6,8 @@ package com.edu.jewelry.controller.rest;
 
 import com.edu.jewelry.model.Material;
 import com.edu.jewelry.service.material.impls.MaterialServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/api/materials")
 @RestController
 public class MaterialRestController {
+    Logger logger = LogManager.getLogger(MaterialRestController.class);
     @Autowired
     MaterialServiceImpl materialService;
 
@@ -26,30 +29,35 @@ public class MaterialRestController {
     @CrossOrigin
     @RequestMapping(value = "", method = RequestMethod.GET)
     List<Material> getMaterialsList() {
+        logger.info("info: go to materials list method");
         return materialService.getAll();
     }
 
     @CrossOrigin
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     Material getMaterial(@PathVariable("id") String id) {
+        logger.info("info: go to materials get method");
         return materialService.get(id);
     }
 
     @CrossOrigin
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     Material deleteMaterial(@PathVariable("id") String id){
+        logger.info("info: go to materials delete method");
         return materialService.delete(id);
     }
 
     @CrossOrigin
     @RequestMapping(value = "", method = RequestMethod.POST)
     Material createMaterial(@RequestBody Material material){
+        logger.info("info: go to materials create method");
         return materialService.create(material);
     }
 
     @CrossOrigin
     @RequestMapping(value = "", method = RequestMethod.PUT)
     Material updateMaterial(@RequestBody Material material){
+        logger.info("info: go to material update method");
         return materialService.update(material);
     }
 }
